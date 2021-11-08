039599a86c2d113e49d21067dd8fde4cdb7a55b6
juandrepanther <janis@innergang.com>
2021-11-08 18:24:50 +0200

    small corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import {useEffect} from 'react'
-import {useDispatch, useSelector} from "react-redux"
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux"
 import { fetchData } from './redux/valueReducer';
 import { RootState } from './redux/store';
 
 import '../src/App.css'
 
-function App() {
+function App() { 
   const dispatch = useDispatch()
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(fetchData())
-  },[dispatch])
-
-const fetchedData = useSelector((state: RootState) => state.value.value)
-console.log(fetchedData);
+  }, [dispatch])
+ 
+  const fetchedData = useSelector((state: RootState) => state.value.value)
+  console.log(fetchedData);
 
 
   return (
@@ -21,11 +21,11 @@ console.log(fetchedData);
       <div className='title'>TODO LIST via Redux Toolkit and TypeScript</div>
       <div className='title2'>Using createAsyncThunk</div>
       <div className="container">
-        {fetchedData.map(todo=>{
+        {fetchedData.map(todo => {
           return (
-            
+ 
             <div key={todo.id} className='todo-item'>{todo.id} TASK : {todo.title}</div>
-            
+
           )
         })}
       </div>
